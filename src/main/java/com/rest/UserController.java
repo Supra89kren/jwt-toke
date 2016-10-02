d133de5d7735ac392193f1b1ef43de96d0149762
supra89kren <supra89kren@gmail.com>
2016-10-02 22:26:10 +0300

    removed trash

--- src/main/java/com/rest/UserController.java
+++ src/main/java/com/rest/UserController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * Created by Администратор on 02.10.2016.
- */
 @RestController
 public class UserController {
 
