521286b4b32e3619443719e7c3d5f61d1c7cbe94
supra89kren <supra89kren@gmail.com>
2016-10-02 22:22:55 +0300

    removed trash

--- src/main/java/com/repositories/UserRepository.java
+++ src/main/java/com/repositories/UserRepository.java
@@ -4,9 +4,6 @@ import com.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by Администратор on 02.10.2016.
- */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
